6a3a6f9c00fc2df5fef8261b3bb0fef5105d1c46
Peter Trost <p.trost93@gmail.com>
2024-09-25 12:48:23 +0200

    feat: Add test app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/github/actions/testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.github_actions_testapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:github_actions_testapp/app/app.dart';
+import 'package:github_actions_testapp/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
